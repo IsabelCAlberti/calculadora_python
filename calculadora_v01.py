271bec23c5740ac013cac7fc140a9f63d6a0fbea
Isabel Fagundes <isabelalberti84@gmail.com>
2025-09-08 21:04:32 -0300

    Finalizando

--- calculadora_v01.py
+++ calculadora_v01.py
@@ -2,4 +2,29 @@ def calculadora():
     print("=== CALCULADORA ===")
     print("Operações: +  -  *  /  ** (potência)")
 
-    
+    while True:
+        n1 = float(input("Digite o primeiro número: "))
+        op = input("Digite a operação: ")
+        n2 = float(input("Digite o segundo número: "))
+
+        if op == "+":
+            print("Resultado:", n1 + n2)
+        elif op == "-":
+            print("Resultado:", n1 - n2)
+        elif op == "*":
+            print("Resultado:", n1 * n2)
+        elif op == "/":
+            if n2 != 0:
+                print("Resultado:", n1 / n2)
+            else:
+                print("Erro: divisão por zero!")
+        elif op == "**":
+            print("Resultado:", n1 ** n2)
+        else:
+            print("Operação inválida!")
+
+        cont = input("Deseja continuar? (s/n): ")
+        if cont.lower() != "s":
+            break
+
+calculadora()
